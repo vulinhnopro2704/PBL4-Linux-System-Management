cf320d25e51c81b5cae8966fb4e434ff968b3b41
vulinhnopro2704 <vulinhtruong79@gmail.com>
2024-08-25 15:03:42 +0700

    PBL4-6 Initial Folder structure

--- src/main/java/com/myapp/util/SSHConnection.java
+++ src/main/java/com/myapp/util/SSHConnection.java
@@ -4,26 +4,24 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 
 public class SSHConnection {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 2222;  // Sửa port thành 2222
+        String user = "root";
+        String password = "rootpassword";
 
-    private Session session;
-
-    public void connect(String username, String password, String host, int port) {
         try {
             JSch jsch = new JSch();
-            session = jsch.getSession(username, host, port);
+            Session session = jsch.getSession(user, host, port);
             session.setPassword(password);
+
             session.setConfig("StrictHostKeyChecking", "no");
+
             session.connect();
-            System.out.println("Connected to " + host);
+            System.out.println("Kết nối thành công!");
+            session.disconnect();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    public void disconnect() {
-        if (session != null && session.isConnected()) {
-            session.disconnect();
-            System.out.println("Disconnected");
         }
     }
 }
