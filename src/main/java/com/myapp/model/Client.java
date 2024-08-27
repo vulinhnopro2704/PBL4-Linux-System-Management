bd6da21b86ab8f187915187f25f86c3d2b549e6d
lvtruong <vulinhtruong79@gmail.com>
2024-08-27 21:53:57 +0700

    PBL4-6 Bugfix

--- src/main/java/com/myapp/model/Client.java
+++
@@ -1,9 +0,0 @@
-package com.myapp.model;
-
-
-public class Client {
-    private String name;
-    private String ipAddress;
-
-    // Constructor, getters, setters
-}
--- src/main/java/com/myapp/model/Server.java
+++
@@ -1,8 +0,0 @@
-package com.myapp.model;
-
-public class Server {
-    private String name;
-    private String ipAddress;
-
-    // Constructor, getters, setters
-}
