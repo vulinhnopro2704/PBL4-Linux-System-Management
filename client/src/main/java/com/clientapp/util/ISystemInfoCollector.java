61b74b7cae94a7d475b4541228133a14b2fd642f
vulinhnopro2704 <vulinhtruong79@gmail.com>
2024-09-02 22:59:39 +0700

    PBL4-10 Get all client information by oshi

---
+++ client/src/main/java/com/clientapp/util/ISystemInfoCollector.java
@@ -0,0 +1,48 @@
+package com.clientapp.util;
+
+import java.util.List;
+
+public interface ISystemInfoCollector {
+
+    /**
+     * Retrieves information about the CPU.
+     *
+     * @return A string containing CPU details.
+     */
+    String getCpuInfo();
+
+    /**
+     * Retrieves information about the memory.
+     *
+     * @return A string containing memory details.
+     */
+    String getMemoryInfo();
+
+    /**
+     * Retrieves information about the operating system.
+     *
+     * @return A string containing OS details.
+     */
+    String getOsInfo();
+
+    /**
+     * Retrieves information about the network interfaces.
+     *
+     * @return A list of strings, each containing details of a network interface.
+     */
+    List<String> getNetworkInfo();
+
+    /**
+     * Retrieves the hostname of the current machine.
+     *
+     * @return The hostname as a string.
+     */
+    String getHostnameInfo();
+
+    /**
+     * Retrieves information about disk usage.
+     *
+     * @return A list of strings, each containing details of a disk.
+     */
+    List<String> getDiskUsageInfo();
+}
--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.clientapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires transitive javafx.graphics;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
@@ -9,7 +10,10 @@ module com.clientapp {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires com.github.oshi;
 
     opens com.clientapp to javafx.fxml;
     exports com.clientapp;
+    exports com.clientapp.controller;
+    opens com.clientapp.controller to javafx.fxml;
 }
