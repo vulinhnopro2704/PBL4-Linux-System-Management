8f47514e4b8ce419004963648f4dd74e8bd98df2
vulinhnopro2704 <vulinhtruong79@gmail.com>
2024-10-28 11:36:23 +0700

    PBL-27 Commit for resolve conflict Develop into main

--- server/src/main/java/com/serverapp/controller/view/AppController.java
+++ server/src/main/java/com/serverapp/controller/view/AppController.java
@@ -21,22 +21,6 @@ public class AppController implements IController {
     @Setter
     private String currentClientIp;
 
-    public static AppController getInstance() {
-        if (_INSTANCE == null) {
-            _INSTANCE = new AppController();
-        }
-        return _INSTANCE;
-    }
-
-    @FXML
-    public void initialize() {
-        _INSTANCE = this;
-        SocketManager.getInstance().startListening();
-        btnSystemMonitoring.getStyleClass().setAll("active-button");
-        setFxmlPath("/view/main-system-view.fxml");
-        loadPage(this.fxmlPath);
-    }
-
     @FXML
     public AnchorPane contentArea;
 
@@ -61,6 +45,21 @@ public class AppController implements IController {
     @Setter
     private String fxmlPath;
 
+    public static AppController getInstance() {
+        if (_INSTANCE == null) {
+            _INSTANCE = new AppController();
+        }
+        return _INSTANCE;
+    }
+
+    @FXML
+    public void initialize() {
+        _INSTANCE = this;
+        SocketManager.getInstance().startListening();
+        btnSystemMonitoring.getStyleClass().setAll("active-button");
+        loadPage("/view/main-system-view.fxml");
+    }
+
     // Reset button styles to default
     private void resetButtonStyles() {
         btnSystemMonitoring.getStyleClass().setAll("normal-button");
@@ -73,57 +72,64 @@ public class AppController implements IController {
 
     @FXML
     private void handleSystemMonitoring(MouseEvent event) {
-        resetButtonStyles();
-        btnSystemMonitoring.getStyleClass().setAll("active-button");
-        setFxmlPath("/view/main-system-view.fxml");
-        loadPage(this.fxmlPath);
+        if (!fxmlPath.equals("/view/main-system-view.fxml")){
+            resetButtonStyles();
+            btnSystemMonitoring.getStyleClass().setAll("active-button");
+            loadPage("/view/main-system-view.fxml");
+        }
     }
 
     @FXML
     private void handleCommandPrompt(MouseEvent event) {
-        resetButtonStyles();
-        btnCommandPrompt.getStyleClass().setAll("active-button");
-        setFxmlPath("/view/main-command-view.fxml");
-        loadPage(this.fxmlPath);
+        if (!fxmlPath.equals("/view/main-command-view.fxml")){
+            resetButtonStyles();
+            btnCommandPrompt.getStyleClass().setAll("active-button");
+            loadPage("/view/main-command-view.fxml");
+        }
     }
 
     @FXML
     private void handleFileAndDirectory(MouseEvent event) {
-        resetButtonStyles();
-        btnFileAndDirectory.getStyleClass().setAll("active-button");
-        setFxmlPath("/view/main-filedirectory-view.fxml");
-        loadPage(this.fxmlPath);
+        if (!fxmlPath.equals("/view/main-filedirectory-view.fxml")){
+            resetButtonStyles();
+            btnFileAndDirectory.getStyleClass().setAll("active-button");
+            loadPage("/view/main-filedirectory-view.fxml");
+        }
     }
 
     @FXML
     private void handleInstallAndUpdate(MouseEvent event) {
-        resetButtonStyles();
-        btnInstallAndUpdate.getStyleClass().setAll("active-button");
-        setFxmlPath("/view/main-install-view.fxml");
-        loadPage(this.fxmlPath);
+        if (!fxmlPath.equals("/view/main-install-view.fxml")){
+            resetButtonStyles();
+            btnInstallAndUpdate.getStyleClass().setAll("active-button");
+            loadPage("/view/main-install-view.fxml");
+        }
     }
 
     @FXML
     private void handleSecurityManagement(MouseEvent event) {
-        resetButtonStyles();
-        btnSecurityManagement.getStyleClass().setAll("active-button");
-        setFxmlPath("/view/main-security-view.fxml");
-        loadPage(this.fxmlPath);
+        if (!fxmlPath.equals("/view/main-security-view.fxml")){
+            resetButtonStyles();
+            btnSecurityManagement.getStyleClass().setAll("active-button");
+            loadPage("/view/main-security-view.fxml");
+        }
     }
 
     @FXML
     private void handleSetting(MouseEvent event) {
-        resetButtonStyles();
-        btnSetting.getStyleClass().setAll("active-button");
-        setFxmlPath("/view/main-setting-view.fxml");
-        loadPage(this.fxmlPath);
+        if (!fxmlPath.equals("/view/main-setting-view.fxml")){
+            resetButtonStyles();
+            btnSetting.getStyleClass().setAll("active-button");
+            loadPage("/view/main-setting-view.fxml");
+        }
     }
 
     // Method to load pages into the contentArea
     public void loadPage(String fxmlPath) {
-        if (fxmlPath != null) {
+        if (fxmlPath != null && !fxmlPath.isEmpty() && !fxmlPath.equals(this.fxmlPath)) {
             try {
                 System.out.println(fxmlPath);
+                setFxmlPath(fxmlPath);
                 if (currentController != null) {
                     currentController.stop();
                 }
