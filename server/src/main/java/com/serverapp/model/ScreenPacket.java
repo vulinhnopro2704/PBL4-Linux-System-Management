27b9aa1721837fbd88866a49d62fb101fa68158b
vulinhnopro2704 <vulinhtruong79@gmail.com>
2024-11-03 18:30:29 +0700

    PBL4-25 Add Screen Packet

---
+++ server/src/main/java/com/serverapp/model/ScreenPacket.java
@@ -0,0 +1,44 @@
+package com.serverapp.model;
+
+import lombok.Getter;
+
+import java.io.*;
+
+@Getter
+public class ScreenPacket {
+    @Getter
+    private int totalChunks;
+    private int chunkIndex;
+    private int length;
+    @Getter
+    private byte[] data;
+
+    public ScreenPacket(int totalChunks, int chunkIndex, int length, byte[] data) {
+        this.totalChunks = totalChunks;
+        this.chunkIndex = chunkIndex;
+        this.length = length;
+        this.data = data;
+    }
+
+    public byte[] toByteArray() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+        dos.writeInt(totalChunks);
+        dos.writeInt(chunkIndex);
+        dos.writeInt(length);
+        dos.write(data);
+        return baos.toByteArray();
+    }
+
+    public static ScreenPacket fromByteArray(byte[] bytes) throws IOException {
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+        DataInputStream dis = new DataInputStream(bais);
+        int totalChunks = dis.readInt();
+        int chunkIndex = dis.readInt();
+        int length = dis.readInt();
+        byte[] data = new byte[dis.available()];
+        dis.readFully(data);
+        return new ScreenPacket(totalChunks, chunkIndex, length, data);
+    }
+}
+
