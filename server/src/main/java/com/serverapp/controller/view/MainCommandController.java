7afe7324a1ef7871c77a6c6d1625fd543b5d78a6
vulinhnopro2704 <vulinhtruong79@gmail.com>
2024-11-08 21:16:26 +0700

    PBL-27 Commit After refactor code
    PBL-27 Commit for refactor code
    
    PBL-27 Update UI for Command Screen

--- server/src/main/java/com/serverapp/controller/view/MainCommandController.java
+++ server/src/main/java/com/serverapp/controller/view/MainCommandController.java
@@ -1,187 +1,111 @@
 package com.serverapp.controller.view;
 
-import java.io.*;
-import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
-
-
 import com.serverapp.controller.IController;
-import com.serverapp.enums.RequestType;
-import com.serverapp.model.ClientCredentials;
-import com.serverapp.socket.SocketManager;
-
-import static com.serverapp.util.AlertHelper.showAlert;
-
-import com.serverapp.util.CurrentType;
+import com.serverapp.database.Redis;
+import com.serverapp.model.ClientCommnandRow;
+import com.serverapp.service.IClientCommand;
+import com.serverapp.service.implement.ClientCommand;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainCommandController implements IController {
+    @FXML
+    private TableColumn<ClientCommnandRow, Boolean> checkboxColumn;
 
     @FXML
-    private ListView<CheckBox> clientListView;
+    private ListView<String> clientListView;
 
     @FXML
     private TextArea txtAreaCommand;
 
     @FXML
-    private TableView<CheckBox> tableClient;
+    private TableView<ClientCommnandRow> tableClient;
+
+    @FXML
+    private TableColumn<ClientCommnandRow, String> desktopNameColumn;
+    @FXML
+    private TableColumn<ClientCommnandRow, String> ipAddressColumn;
+    @FXML
+    private TableColumn<ClientCommnandRow, String> macAddressColumn;
 
     @FXML
     private TextArea txtAreaTerminalLogs;
 
-    private boolean isWaitingForResponse = false;
-
-    private ObservableList<CheckBox> clientList = FXCollections.observableArrayList();
-    private ExecutorService executor = Executors.newCachedThreadPool();
-    private Boolean isRunning;
+    private final ObservableList<String> clientList = FXCollections.observableArrayList();
+    private final IClientCommand clientCommandService = new ClientCommand(this);
 
     @FXML
     public void initialize() {
-        try {
-            CurrentType.getInstance().setType(RequestType.COMMAND);
-            isRunning = true;
-            Platform.runLater(() -> {
-                HashMap<String, ClientCredentials> clients = SocketManager.getInstance().getAllClientCredentials();
-                clients.forEach((ip, clientData) -> {
-                    CheckBox clientCheckBox = new CheckBox(ip);
-                    clientList.add(clientCheckBox);
-                    // Listen for responses from the client in a separate thread
-                    BufferedWriter writer = null;
-                    try {
-                        writer = new BufferedWriter(new OutputStreamWriter(clientData.getOutputStream()));
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    try {
-                        writer.write(RequestType.COMMAND + "\n");
-                        writer.flush();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    executor.submit(() -> listenForResponse(clientData.getSocket()));
-                });
-                clientListView.setItems(clientList);
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        clientCommandService.initialize();
     }
 
     @Override
     public void update() {
+    }
 
+    public void setupTableColumns(ObservableList<ClientCommnandRow> data) {
+        checkboxColumn.setCellValueFactory(cellData -> cellData.getValue().checkboxProperty());
+        checkboxColumn.setCellFactory(CheckBoxTableCell.forTableColumn(checkboxColumn));
+        checkboxColumn.setEditable(true);
+
+        desktopNameColumn.setCellValueFactory(new PropertyValueFactory<>("desktopName"));
+        ipAddressColumn.setCellValueFactory(new PropertyValueFactory<>("ipAddress"));
+        macAddressColumn.setCellValueFactory(new PropertyValueFactory<>("macAddress"));
+
+        checkboxColumn.setResizable(false);
+        checkboxColumn.setSortable(false);
+        desktopNameColumn.setResizable(false);
+        desktopNameColumn.setSortable(false);
+        ipAddressColumn.setResizable(false);
+        ipAddressColumn.setSortable(false);
+        macAddressColumn.setResizable(false);
+        macAddressColumn.setSortable(false);
+
+        tableClient.setItems(data);
+        tableClient.setEditable(true);
+    }
+
+    public void addClientToList(String ip) {
+        clientList.add(ip);
+        clientListView.setItems(clientList);
+        clientListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                updateConsole();
+            }
+        });
+    }
+
+    public void updateConsole() {
+        String selectedClient = clientListView.getSelectionModel().getSelectedItem();
+        if (selectedClient == null) {
+            return;
+        }
+        String consoleLogs = Redis.getInstance().getConsoleLogs(selectedClient);
+        Platform.runLater(() -> txtAreaTerminalLogs.setText(consoleLogs));
     }
 
     @Override
     public void stop() {
-        close();
-    }
-
-    private void listenForResponse(Socket clientSocket) {
-        try {
-            while (isRunning) {
-                // Receive encrypted response from client
-                String response;
-                if (isRunning)
-                    response = SocketManager.getInstance().receiveDecryptedMessage(clientSocket.getInetAddress().getHostAddress());
-                else {
-                    break;
-                }
-                if (response == null || response.isEmpty()) {
-                    continue;
-                }
-                log("Response from client (" + clientSocket.getInetAddress().getHostAddress() + "): " + response);
-                isWaitingForResponse = false;
-            }
-        } catch (IOException e) {
-            if (clientSocket.isClosed()) {
-                log("(Listen for Response) Client disconnected: " + clientSocket.getInetAddress().getHostAddress());
-            } else {
-                log("Lost connection to client: " + clientSocket.getInetAddress().getHostAddress());
-                e.printStackTrace();
-            }
-        } catch (Exception e) {
-            log("Error receiving response from client: " + clientSocket.getInetAddress().getHostAddress() + " - " + e.getMessage());
-            e.printStackTrace();
-        }
+        clientCommandService.close();
     }
 
     @FXML
-    private void sendCommand() throws Exception {
-        if (!isRunning) {
-            System.out.println("Client Command has stopped");
-            return;
-        }
-
-        if (isWaitingForResponse) {
-            log("Waiting for response from client. Please wait...");
-            showAlert(Alert.AlertType.CONFIRMATION, "Waiting for response", "Please wait", "Waiting for response from client. Please wait...");
-            return;
-        }
+    public void sendCommand() throws Exception {
         String command = txtAreaCommand.getText().trim();
-        if (command.isEmpty()) return;
-        List<CheckBox> checkedClients = clientList.stream()
-                .filter(CheckBox::isSelected)
-                .collect(Collectors.toList());
-
-        if (checkedClients.isEmpty()) {
-            showAlert(Alert.AlertType.WARNING, "No client selected", "Please select at least one client", "Please select at least one client to send the command to.");
-            return;
-        }
-
-        for (CheckBox clientCheckBox : checkedClients) {
-                String clientAddress = clientCheckBox.getText();
-                // Mã hóa và gửi lệnh
-                log("Sending command to " + clientAddress);
-                SocketManager.getInstance().sendEncryptedMessage(command, clientAddress);
-                log("Sent command to " + clientAddress + " Waiting for response...");
-                isWaitingForResponse = true;
-        }
+        clientCommandService.sendCommand(command, tableClient, txtAreaTerminalLogs);
     }
 
-    private void log(String message) {
+    public void log(String message) {
         String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
         String logEntry = "[" + timestamp + "] " + message;
         Platform.runLater(() -> txtAreaTerminalLogs.appendText(logEntry + "\n"));
-    }
-
-
-    void close() {
-        System.out.println("Close Client Command Screen");
-        HashMap<String, ClientCredentials> clients = SocketManager.getInstance().getAllClientCredentials();
-        clients.forEach((ip, clientData) -> {
-            BufferedWriter writer = null;
-            try {
-                writer = new BufferedWriter(new OutputStreamWriter(clientData.getOutputStream()));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                writer.write(RequestType.EXIT_COMMNAD_SCREEN + "\n");
-                writer.flush();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        isRunning = false;
-        isWaitingForResponse = false;
-        executor.shutdown();
-        executor.shutdownNow();
-        System.out.println("Closed client command screen");
     }
 }
---
+++ server/src/main/java/com/serverapp/service/implement/ClientCommand.java
@@ -0,0 +1,175 @@
+package com.serverapp.service.implement;
+
+import com.serverapp.controller.view.MainCommandController;
+import com.serverapp.database.Redis;
+import com.serverapp.enums.RequestType;
+import com.serverapp.model.ClientCard;
+import com.serverapp.model.ClientCommnandRow;
+import com.serverapp.model.ClientCredentials;
+import com.serverapp.service.IClientCommand;
+import com.serverapp.socket.SocketManager;
+import com.serverapp.util.CurrentType;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+import static com.serverapp.util.AlertHelper.showAlert;
+
+public class ClientCommand implements IClientCommand {
+    private final MainCommandController controller;
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+    private boolean isRunning;
+    private boolean isWaitingForResponse = false;
+
+    public ClientCommand(MainCommandController controller) {
+        this.controller = controller;
+    }
+
+    public void initialize() {
+        try {
+            CurrentType.getInstance().setType(RequestType.COMMAND);
+            isRunning = true;
+            Platform.runLater(() -> {
+                List<ClientCard> clientCards = Redis.getInstance().getAllClientCard();
+                ObservableList<ClientCommnandRow> data = FXCollections.observableArrayList(
+                        clientCards.stream().map(clientCard -> new ClientCommnandRow(
+                                false,
+                                clientCard.getHostName(),
+                                clientCard.getIpAddress(),
+                                clientCard.getMacAddress()
+                        )).collect(Collectors.toList())
+                );
+
+                data.add(new ClientCommnandRow(false, "DummyHost1", "192.168.0.101", "00:0a:95:9d:68:16"));
+                data.add(new ClientCommnandRow(false, "DummyHost2", "192.168.0.102", "00:0a:95:9d:68:17"));
+
+                controller.setupTableColumns(data);
+
+                HashMap<String, ClientCredentials> clients = SocketManager.getInstance().getAllClientCredentials();
+                clients.forEach((ip, clientData) -> {
+                    controller.addClientToList(ip);
+                    BufferedWriter writer = null;
+                    try {
+                        writer = new BufferedWriter(new OutputStreamWriter(clientData.getOutputStream()));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    try {
+                        writer.write(RequestType.COMMAND + "\n");
+                        writer.flush();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    executor.submit(() -> listenForResponse(clientData.getSocket()));
+                });
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendCommand(String command, TableView<ClientCommnandRow> tableClient, TextArea txtAreaTerminalLogs) throws Exception {
+        if (!isRunning) {
+            System.out.println("Client Command has stopped");
+            return;
+        }
+
+        if (isWaitingForResponse) {
+            controller.log("Waiting for response from client. Please wait...");
+            showAlert(Alert.AlertType.CONFIRMATION, "Waiting for response", "Please wait", "Waiting for response from client. Please wait...");
+            return;
+        }
+
+        if (command.isEmpty()) return;
+
+        List<ClientCommnandRow> checkedClients = tableClient.getItems().stream()
+                .filter(ClientCommnandRow::isCheckbox)
+                .toList();
+
+        if (checkedClients.isEmpty()) {
+            showAlert(Alert.AlertType.WARNING, "No client selected", "Please select at least one client", "Please select at least one client to send the command to.");
+            return;
+        }
+
+        for (ClientCommnandRow clientRow : checkedClients) {
+            String clientAddress = clientRow.getIpAddress();
+            controller.log("Sending command to " + clientAddress);
+            SocketManager.getInstance().sendEncryptedMessage(command, clientAddress);
+            controller.log("Sent command to " + clientAddress + " Waiting for response...");
+            isWaitingForResponse = true;
+        }
+    }
+
+    public void listenForResponse(Socket clientSocket) {
+        try {
+            while (isRunning) {
+                String response;
+                if (isRunning)
+                    response = SocketManager.getInstance().receiveDecryptedMessage(clientSocket.getInetAddress().getHostAddress());
+                else {
+                    break;
+                }
+                if (response == null || response.isEmpty()) {
+                    continue;
+                }
+                if (!response.trim().isEmpty()) {
+                    String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+                    String logEntry = "[" + timestamp + "] " + response;
+                    Redis.getInstance().appendConsoleLogs(clientSocket.getInetAddress().getHostAddress(), logEntry);
+                    controller.updateConsole();
+                    isWaitingForResponse = false;
+                }
+            }
+        } catch (IOException e) {
+            if (clientSocket.isClosed()) {
+                controller.log("(Listen for Response) Client disconnected: " + clientSocket.getInetAddress().getHostAddress());
+            } else {
+                controller.log("Lost connection to client: " + clientSocket.getInetAddress().getHostAddress());
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            controller.log("Error receiving response from client: " + clientSocket.getInetAddress().getHostAddress() + " - " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        System.out.println("Close Client Command Screen");
+        HashMap<String, ClientCredentials> clients = SocketManager.getInstance().getAllClientCredentials();
+        clients.forEach((ip, clientData) -> {
+            BufferedWriter writer = null;
+            try {
+                writer = new BufferedWriter(new OutputStreamWriter(clientData.getOutputStream()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                writer.write(RequestType.EXIT_COMMNAD_SCREEN + "\n");
+                writer.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        isRunning = false;
+        isWaitingForResponse = false;
+        executor.shutdown();
+        executor.shutdownNow();
+        System.out.println("Closed client command screen");
+    }
+}
